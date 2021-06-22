723ac0626ae13087f544e5894ea4459966421ae5
fpjnijweide <fpjnijweide@protonmail.com>
2021-06-22 22:41:33 +0100

    csv_to_pdb now takes semicolons and commas without problems; no longer fails on integer data instead of floats

--- csv_to_pdb.py
+++ csv_to_pdb.py
@@ -6,7 +6,7 @@ import scipy
 
 import re
 def natural_key(string_):
-    if isinstance(string_,np.float64) or isinstance(string_,bool) or isinstance(string_,int) or isinstance(string_,float):
+    if isinstance(string_,np.number) or isinstance(string_,bool) or isinstance(string_,int) or isinstance(string_,float):
         return string_
     else:
         """See https://blog.codinghorror.com/sorting-for-humans-natural-sort-order/"""
@@ -14,7 +14,10 @@ def natural_key(string_):
 
 def make_pdb(file_string,sampling_density):
     df = pd.read_csv(file_string,sep=';')
+    df2 = pd.read_csv(file_string,sep=',')
 
+    if df.columns.size < df2.columns.size:
+        df = df2
 
     str_cols = df.select_dtypes(include=['object']).columns
     df.loc[:, str_cols] = df.loc[:, str_cols].fillna('NULL')
@@ -32,15 +35,17 @@ def make_pdb(file_string,sampling_density):
         unique_entries_in_coli = coli_nonan.unique()
         unique_entries_in_coli = np.array(sorted(unique_entries_in_coli,key=natural_key))
         # unique_entries_in_coli.sort()
-        if coli_nonan.dtype != np.float64:
+        if not np.issubdtype(coli_nonan.dtype,np.number):
+            # if not numeric, just use the unique entries for binning
             sizes_sorted[i] = unique_entries_in_coli.size
             bins[i] = unique_entries_in_coli
         elif sampling_density is not None:
-            # use predefined sampling density
+            # if numeric but with predefined sampling density, use that
             bins_for_coli = np.histogram_bin_edges(coli_nonan,bins=sampling_density)
             sizes_sorted[i] = bins_for_coli.size-1
             bins[i] = bins_for_coli
         else:
+            # else, determine sampling density heuristically
             bins_for_coli = np.histogram_bin_edges(coli_nonan,bins='auto')
             if unique_entries_in_coli.size <= bins_for_coli.size:
                 sizes_sorted[i] = unique_entries_in_coli.size
@@ -100,7 +105,8 @@ def make_pdb(file_string,sampling_density):
     return df,sizes_sorted,hard_evidence
 
 if __name__ == "__main__":
-    df, sizes_sorted, hard_evidence = make_pdb("surgical_case_durations.csv",None)
+    # df, sizes_sorted, hard_evidence = make_pdb("surgical_case_durations.csv",None)
+    df, sizes_sorted, hard_evidence = make_pdb("Dataset - LBP RA.csv",None)
     hard_evidence.to_pickle("surgical_case_durations.pdb")
     hard_evidence2 = pd.read_pickle("surgical_case_durations.pdb")
     print(hard_evidence.equals(hard_evidence2))
